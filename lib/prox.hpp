d28b7fa8e321712558d0019c42223be88dbed9db
Kelvin-Ng <kelvin9302104@gmail.com>
2016-11-01 13:06:42 +0800

    Use FeaValIterator to implement one prox_l1() for both dense and sparse vector

--- lib/prox.hpp
+++ lib/prox.hpp
@@ -11,36 +11,20 @@ inline Vector<T, is_sparse> prox_l2(const Vector<T, is_sparse>& y, T alpha, T la
     return y / (1 + alpha * lambda);
 }
 
-template<typename T>
-inline Vector<T, false> prox_l1(const Vector<T, false>& y, T alpha, T lambda) {
-    Vector<T, false> res(y.get_feature_num());
+template <typename T, bool is_sparse>
+Vector<T, is_sparse> prox_l1(const Vector<T, is_sparse>& y, T alpha, T lambda) {
+    Vector<T, is_sparse> res(y.get_feature_num());
     T lambda_ = alpha * lambda;
 
-    for (int i = 0; i < y.get_feature_num(); i++) {
-        if (std::abs(y[i]) <= lambda_) {
-            res[i] = 0;
-        } else if (y[i] > 0){
-            res[i] = y[i] - lambda_;
+    for (auto it = y.begin_feaval(); it != y.end_feaval(); ++it) {
+        const auto&& entry = *it;
+
+        if (std::abs(entry.val) <= lambda_) {
+            res.set(entry.fea, 0);
+        } else if (entry.val > 0){
+            res.set(entry.fea, entry.val - lambda_);
         } else {
-            res[i] = y[i] + lambda_;
-        }
-    }
-
-    return res;
-}
-
-template<typename T>
-inline Vector<T, true> prox_l1(const Vector<T, true>& y, T alpha, T lambda) {
-    Vector<T, true> res(y.get_feature_num());
-    T lambda_ = alpha * lambda;
-
-    for (auto& entry : y) {
-        if (std::abs(entry.second) <= lambda_) {
-            res.set(entry.first, 0);
-        } else if (entry.second > 0){
-            res.set(entry.first, entry.second - lambda_);
-        } else {
-            res.set(entry.first, entry.second + lambda_);
+            res.set(entry.fea, entry.val + lambda_);
         }
     }
 
